24ae5ec387d95a430392fbb102cc0ca7c1b92713
JakkiFx <jackcielfelix555@gmail.com>
2023-09-30 08:57:49 -0300

    ruim

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,3 @@
-import { BitlyService } from './../../services/bitly.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,27 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  inputValue: string;
-  noValid = false;
 
-  constructor(private bitlyService: BitlyService) {}
+  constructor() { }
 
-  ngOnInit() {}
-
-  shortenUrl(): void {
-    this.bitlyService.shortenUrl(this.inputValue).subscribe((response) => {
-      console.log('URL encurtada:', response.link);
-    });
-  }
-
-  submit(): void {
-    if (this.inputValue) {
-      this.shortenUrl();
-      console.log(`lkmlsdsdflmsdf`)
-    } else {
-      this.noValid = true;
-      console.log(`lakm`)
-    }
+  ngOnInit() {
   }
 
 }
